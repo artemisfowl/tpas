c6eafea78f2801ccd72af398607487ea6577f591
artemisfowl <skb655952@gmail.com>
2023-04-16 18:29:50 +0530

    Added code for properly parsing the config file data

--- run.py
+++ run.py
@@ -15,9 +15,8 @@ if __name__ == "__main__":
     fw_config = GeneralConfig()
     fw_config.read_config(config_file_path=f"{getcwd()}{sep}config{sep}tpas.ini")
     scribe.set_log_level(DEBUG if fw_config.get_config(section_name="framework", option_name="enabledebug") == 1 else INFO)
-    scribe.enable_filelogging(bool(fw_config.get_config(section_name="framework", option_name="enablefile")))
-    # fixme: removing the file handler when the same is not disabled in the configuration file - needs to be checked.
-    scribe.enable_streamlogging(bool(fw_config.get_config(section_name="framework", option_name="enablestream")))
+    scribe.enable_filelogging(bool(int(fw_config.get_config(section_name="framework", option_name="enablefile")))) # type: ignore
+    scribe.enable_streamlogging(bool(int(fw_config.get_config(section_name="framework", option_name="enablestream")))) # type: ignore
     
     info("Parsing the CLI arguments")
     debug(f"Current working directory : {getcwd()}")
